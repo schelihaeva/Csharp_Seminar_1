801762af09d7832c82da6791769f3d77b4a8b8ff
Anastasya <gbshell2022@gmail.com>
2023-10-10 12:36:43 +0300

    Добавила идеальное решение Task_001

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -3,21 +3,41 @@
 //  a = 2; b = 10 -> max = 10
 // a = -9; b = -3 -> max = -3
 //using System.Globalization;
-
-Console.WriteLine("Введите 2 числа и мы тебе скажем какое из них больше");
+// Мое решение
+// Console.WriteLine("Введите 2 числа и мы тебе скажем какое из них больше");
+// System.Console.WriteLine("Введите свое первое число");
+// int a = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Введите второе число:");
+//int b = Convert.ToInt32(Console.ReadLine());
+//
+// if (a > b)
+//{
+    //System.Console.WriteLine("больше");
+//}
+//else if(a == b) {
+ //   System.Console.WriteLine("равен");
+//
+//}
+   // else{
+      //  System.Console.WriteLine("меньше");
+   // }
+   // пример идельного решения
+   //if (firstNumber == secondNumber)
+   Console.WriteLine("Введите 2 числа и мы тебе скажем какое из них больше");
 System.Console.WriteLine("Введите свое первое число");
-int a = Convert.ToInt32(Console.ReadLine());
+firstNumber = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введите второе число:");
-int b = Convert.ToInt32(Console.ReadLine());
+secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if (a > b)
-{
-    System.Console.WriteLine("больше");
-}
-else if(a == b) {
-    System.Console.WriteLine("равен");
-
-}
-    else{
-        System.Console.WriteLine("меньше");
-    }
+        if (firstNumber == secondNumber){
+                Console.WriteLine($"Введенные числа равны `{firstNumber}`");
+        }
+        
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine($"Первое число `{firstNumber}` больше чем второе число `{secondNumber}`");
+        }
+        if (firstNumber < secondNumber)
+        {
+            Console.WriteLine($"Первое число `{firstNumber}` меньше чем второе число `{secondNumber}`");
+        }
